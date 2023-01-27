7c0a829bcc65fd30bd71afb96fce274a27fd87a5
Vu Nguyen Tien <gtvvunguye@gmail.com>
2023-01-27 12:27:33 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React, {Suspense, Redirect, Switch} from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Button } from 'reactstrap';
+import './App.scss';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="photo-app">
+    <Suspense fallback={<div>Loading ...</div>}>
+      <BrowserRouter>
+        <Header />
+
+        {/* <Button onClick={handleButtonClick}>Fetch Product List</Button> */}
+
+        <Switch>
+          <Redirect exact from="/" to="/photos" />
+
+          {/* <Route path="/photos" component={Photo} />
+          <Route path="/sign-in" component={SignIn} /> */}
+          {/* <Route component={NotFound} /> */}
+        </Switch>
+      </BrowserRouter>
+    </Suspense>
+  </div>
   );
 }
 
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+      Oopss ... Not found
+    </div>
+  )
+}
+
+export default index
